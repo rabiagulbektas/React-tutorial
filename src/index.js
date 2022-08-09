6fe4b5354db8bc4b95a76fe33e8ad4776940cc7d
DESKTOP-BESFO9P\user <rabiagull.bektas@gmail.com>
2022-08-09 12:03:58 +0300

    Reactstrap bootstrap ile import edildi

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//npm install reactstrap yaptıktan sonra import ile bootstrap'de ekleniyor, ikisinide kullanabiliriz
 import  'bootstrap/dist/css/bootstrap.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
